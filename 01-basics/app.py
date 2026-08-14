85276ee2696d049402567145230c2c7ce8f53193
B1stmode <tima.gamedev@gmail.com>
2026-08-14 10:16:45 +0200

    basics - app.py

---
+++ 01-basics/app.py
@@ -0,0 +1,15 @@
+# Drill: Basic Arithmetic Operations
+
+# drill practicess: 
+# - arithmetic operators
+# - console output
+
+#Time spent: 1 minute
+
+a = 37
+b = 8
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
