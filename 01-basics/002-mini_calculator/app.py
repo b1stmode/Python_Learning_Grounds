1af6f8895c650a04d8f2ce34dc5e783867e578f2
B1stmode <tima.gamedev@gmail.com>
2026-08-20 10:21:27 +0200

    basics - mini calculator

---
+++ 01-basics/002-mini_calculator/app.py
@@ -0,0 +1,18 @@
+# Drill: Mini Calculator
+
+# drill practicess: 
+# - arithmetic operators
+# - console output
+
+#Time spent:  minutes
+
+price = 24
+quantity = 3
+discount = 5
+
+total_price = price * quantity
+final_price = total_price - discount
+print(final_price)
+quantity += 2
+final_price = price * quantity - discount
+print(final_price)
