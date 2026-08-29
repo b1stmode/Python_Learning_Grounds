253d586f16b39d3d133187f49f62202c6d9708e1
B1stmode <tima.gamedev@gmail.com>
2026-08-29 10:04:57 +0200

    basics - user input drills

---
+++ 01-basics/009-age_calculator/app.py
@@ -0,0 +1,15 @@
+# Drill: Age Calculator
+
+# drill practicess: 
+# - logical operators
+# - console input
+# - console output
+
+#Time spent: 1.5 minutes
+
+print("Enter your age: ")
+usr_age = int(input())
+
+age_next_year = usr_age + 1
+
+print(f"Next year you will be {age_next_year} years old.")
---
+++ 01-basics/010-price_calculator/app.py
@@ -0,0 +1,17 @@
+# Drill: Age Calculator
+
+# drill practicess: 
+# - logical operators
+# - console input
+# - console output
+
+#Time spent:  minutes
+
+price = float(input("enter products price: "))
+quantity = int(input("enter products quantity: "))
+
+total = price * quantity
+
+receipt = str(total)
+
+print(receipt)
